9d3d1a39e0a0ce6e9866139f807f4ea594f6ec02
Jalilbwp <llu.exams@gmail.com>
2025-06-20 10:36:30 +0500

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,11 @@
-
+mport React from 'react'
+mport ReactDOM from 'react-dom/client'
+mport App from './App'
+mport {BrowserRouter} from "react-router-dom"
+eactDOM.createRoot(document.getElementById('root')).render(
+ <React.StrictMode>
+   <BrowserRouter>
+     <App />
+   </BrowserRouter>
+ </React.StrictMode>,
+)
