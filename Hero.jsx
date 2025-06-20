9c12ead167d7a4599f612e30bfc82b45e112bced
Jalilbwp <llu.exams@gmail.com>
2025-06-20 11:06:27 +0500

    Update Hero.jsx

--- Hero.jsx
+++ Hero.jsx
@@ -1,20 +1,20 @@
-import { Link } from 
-export default functi
+import { Link } from "react-router-dom"
+export default function Hero() {
 
     return (
-      <div className=
-        <div classNam
-            <div clas
-                <p cl
-                <p cl
-                <p cl
-                    f
-                <Link
-                    R
-                </but
+      <div className="hero">
+        <div className="hero-content">
+            <div className="hero-desc">
+                <p className="title">Little Lemon</p>
+                <p className="subtitle">Chicago</p>
+                <p className="desc">We are a family owned Mediterranean restaurant, 
+                    focused on traditional recipes served with a modern twist.</p>
+                <Link to="/booking" ><button className="reserve-btn btn">
+                    Reserve a table
+                </button></Link>
             </div>
-            <div clas
-            <img clas
+            <div className="img-wrapper">
+            <img className="hero-img" src="../images/hero.png" alt="hero image"/>
             </div>
         </div>
       </div>
