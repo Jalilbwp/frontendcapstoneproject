9778bab1edaa349baae3a80b7bfb392dd6de8b8b
Jalilbwp <llu.exams@gmail.com>
2025-06-20 11:05:59 +0500

    Create Hero.jsx

---
+++ Hero.jsx
@@ -0,0 +1,23 @@
+import { Link } from 
+export default functi
+
+    return (
+      <div className=
+        <div classNam
+            <div clas
+                <p cl
+                <p cl
+                <p cl
+                    f
+                <Link
+                    R
+                </but
+            </div>
+            <div clas
+            <img clas
+            </div>
+        </div>
+      </div>
+    )
+  }
+  
